c1472da6720440ffef31f1ce54757d37103e95d7
Kevin Neag <neagkv@gmail.com>
2017-10-11 10:17:55 -0500

    Part 5 complete

--- part_05/Exercise_09.java
+++ part_05/Exercise_09.java
@@ -10,4 +10,35 @@ package part_05;
 
 class Exercise_09 {
 
-}
+    static void vaMeth(String ... v) {
+        System.out.println("Number of args: "+ v.length);
+        System.out.println("Contents: ");
+
+        StringBuilder buildAString = new StringBuilder();
+
+        for(String a : v){
+            buildAString.append(a);
+        }
+
+        System.out.println(buildAString.toString());
+
+        for (int i = 0; i < v.length; i++)
+            System.out.println("arg " + i + ": " + v[i]);
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+
+        vaMeth("This", "is", "varargs");
+        vaMeth("I", "am", "a", "String");
+        vaMeth("Hello", "World");
+    }
+
+
+    }
+
+
+
+
