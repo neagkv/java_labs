35d7699182b50453bea7bb04f49359cec09c8985
Kevin Neag <neagkv@gmail.com>
2017-10-17 22:13:14 -0500

    Beinging of part 8

---
+++ part_08/Exercise_01.java
@@ -0,0 +1,40 @@
+package part_08;
+
+public class Exercise_01 {
+
+    public static void main(String[] args) {
+        int a, b;
+
+
+        try {
+
+            //place this statement inside the try block, because we believe the error occurs here
+
+            a = 0;
+            b = 365 / a;
+            System.out.println(b);
+            System.out.println("End of try block");
+        }
+
+        catch (ArithmeticException e) {
+
+            // This block will only execute if the arithmetic exception executes
+
+            System.out.println("you attempted to divide by zero");
+        }
+
+        catch (Exception e){
+
+            // generic handler that can handle all exceptions
+            // will only execute if the exception is not previously executed
+
+            System.out.println("Exception Occurred");
+        }
+
+        //confirms that the program continued after reaching the exception
+        System.out.println("You have reached the end of the main method");
+    }
+
+}
+
+
