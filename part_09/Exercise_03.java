737119c8aa6eedf8b3f22e8c55c7215aeeedead4
Kevin Neag <neagkv@gmail.com>
2017-10-21 19:22:51 -0500

    Tulum

--- part_09/Exercise_03.java
+++ part_09/Exercise_03.java
@@ -1,6 +1,38 @@
 package part_09;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 // Demonstrate how to read file using BufferedReader - demonstrate both the read() and readLine() methods()
 
 public class Exercise_03 {
+
+    public static void main(String[] args) {
+
+        BufferedReader br = null;
+
+
+        try{
+           br = new BufferedReader(new FileReader("//sometextfile.txt"));
+
+            String newLine = br.readLine();
+
+            while(newLine != null){
+
+                System.out.println(newLine);
+
+                newLine = br.readLine();
+
+            }
+
+
+        }
+
+        catch (IOException ioe){
+
+        }
+
+
+    }
 }
