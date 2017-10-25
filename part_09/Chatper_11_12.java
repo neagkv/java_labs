2ec002c15f5eb89b150b7bf6b2e181aab98d9573
Kevin Neag <neagkv@gmail.com>
2017-10-25 17:37:04 -0500

    Part 9 complete

--- part_09/Chatper_11_12.java
+++ part_09/Chatper_11_12.java
@@ -3,5 +3,60 @@ package part_09;
 //12. On your own, try adding synchronization to the Queue class developed in previous chapters so that it is safe for
 //multithreaded use.
 
-public class Chatper_11_12 {
+
+class Chapter_11_12 {
+
+    public static void main(String[] args) {
+
+        Chapter_11_12.Queue test = new Chapter_11_12.Queue(101);
+
+        for(int i=0; i<test.size(); i++){
+            test.put(i);
+        }
+
+        for (int i =0; i <test.size(); i++){
+            if(i%2==0) {
+                System.out.println(test.get());
+            } else if (i%2 !=0) {
+                test.get();
+            }
+        }
+
+    }
+
+    static class Queue {
+
+        private int[] q;
+        private int putLocation, getLocation;
+
+
+        Queue(int size) {
+            q = new int[size];
+            putLocation = getLocation = 0;
+        }
+
+        synchronized public void put(int num) {
+            if (putLocation == q.length) {
+                System.out.println("-Queue is full");
+                return;
+            }
+            q[putLocation++] = num;
+            notify();
+            return;
+        }
+
+        synchronized public int get() {
+            if (getLocation == putLocation) {
+                System.out.println("-Queue is empty");
+                return 0;
+            }
+            notify();
+            return q[getLocation++];
+        }
+
+        public int size() {
+
+            return q.length;
+        }
+    }
 }
