713a09eb5daf6fe5f9f8abee972e9a0f6a2c9cea
Kevin Neag <neagkv@gmail.com>
2017-10-26 09:59:13 -0500

    part 9 continued

--- part_11/Bonus.java
+++ part_11/Bonus.java
@@ -1,11 +1,32 @@
 package part_11;
 
-public class Bonus {
-    /*
+
+  /*
  make a new generic method called sumArray that will return the
  sum of all numbers in the array (no matter data type of numbers
  the array has in it)
      */
+
+public class Bonus <T extends Number> {
+
+    T num;
+
+    public Bonus(T n) {
+        n = num;
+    }
+
+    <T> void sumArray(T[] array){
+
+
+
+
+
+
+        }
+
+
+    }
+
 
 
 }
--- part_11/Exercise_02.java
+++ part_11/Exercise_02.java
@@ -4,26 +4,52 @@ package part_11;
  Write a generic method to exchange the positions of two different elements in an array.
  */
 
+public class Exercise_02 {
 
-public class Exercise_02  {
+
+   static <T> void exchangeElements(T[] array, int i, int j) {
+
+        T temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+
+    }
+
+    static void test() {
+
+        String[] stringArray = {"This", "Is", "A", "String", "Array"};
+        exchangeElements(stringArray, 0, 4);
+
+        for(String x : stringArray){
+            System.out.println(x);
+        }
+
+        Integer[] intArray = {1, 2, 3, 4, 5, 6, 7, 8};
+
+        exchangeElements(intArray, 0, 7);
+
+        for(int x : intArray){
+            System.out.println(x);
+        }
+
+        Boolean[] boolArray = {true, true, true, true, false};
+
+        exchangeElements(boolArray, 1, 4);
+
+        for(boolean x : boolArray){
+            System.out.println(x);
+        }
+
+    }
+
 
     public static void main(String[] args) {
 
-        int [] abc;
-
-    }
-
-    void exchangeMeth(int[] abc){
-
-        for(int x : abc
 
 
-        }
+        test();
 
 
     }
-
-
-
-
 }
+
